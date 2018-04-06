5b0d2e1d6f3f163321bf0c303f54dc2f4ca18ca3
lyu201 <38089626+lyu201@users.noreply.github.com>
2018-04-06 15:44:21 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,3 +6,23 @@ This is a temporary script file.
 """
 
 from setuptools import setup
+
+
+
+setup(name='climate',
+
+      version='1.0',
+
+      description='An package for Physics 201 term project',
+
+      url='http://github.com/phys201/climate',
+
+      author='lyu201',
+
+      author_email='lyu201@users.noreply.github.com',
+
+      license='GPLv3',
+
+      packages=['climate'],
+
+      install_requires=['numpy'])
