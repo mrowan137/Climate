7de02394ae065bcf70386133e27ec59cdee064b8
Michael Edward Rowan <MRowan@Michaels-MacBook-Pro-25.local>
2018-04-06 19:39:39 -0400

    add io test

---
+++ climate/tests/test_io.py
@@ -0,0 +1,15 @@
+from climate.io import *#get_example_data_file_path, load_data
+
+import pandas as  pd
+
+import unittest
+from unittest import TestCase
+
+
+class TestIo(TestCase):
+    def test_data_io(self):
+        data = load_data(get_example_data_file_path('global_surface_temp_seaice_air_infer.txt'))
+        assert data.year[0] == 1850
+        
+if __name__ == '__main__':
+    unittest.main()
