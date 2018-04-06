bcb4dfb0826c75608ca563fd5ac35d56745d9e9d
lyu201 <38089626+lyu201@users.noreply.github.com>
2018-04-06 16:08:38 -0400

    Add files via upload

---
+++ climate/test1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  6 15:37:21 2018
+
+@author: aberforth
+"""
+
+import numpy as np
+
+def func1(x):
+    print('This is test1. Result is', np.exp(10.0))
