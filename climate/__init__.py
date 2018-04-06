689dba464b1b1a3ba52e2dce552ef7bb76322e3b
lyu201 <38089626+lyu201@users.noreply.github.com>
2018-04-06 15:40:10 -0400

    Add files via upload

--- climate/__init__.py
+++ climate/__init__.py
@@ -5,3 +5,6 @@ Created on Fri Apr  6 15:36:48 2018
 @author: aberforth
 """
 
+def joke():
+    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+            u'Beiherhund das Oder die Flipperwaldt gersput.')
