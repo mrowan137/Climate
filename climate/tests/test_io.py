cb857a750b18138693c9bfb505e0777a86d1c048
Michael Edward Rowan <MRowan@Michaels-MacBook-Pro-25.local>
2018-04-10 02:28:02 -0400

    add test of tcm temp import

--- climate/tests/test_io.py
+++ climate/tests/test_io.py
@@ -8,8 +8,14 @@ from unittest import TestCase
 
 class TestIo(TestCase):
     def test_data_io(self):
-        data = load_data_temp(get_example_data_file_path('global_surface_temp_seaice_air_infer.txt'))
+        data = load_data_temp(get_example_data_file_path(
+            'global_surface_temp_seaice_air_infer.txt'))
         assert data.year[0] == 1850
+
+        data_tcm = load_tcm_temp(get_example_data_file_path(
+            'TempChange.dat', data_dir='trad_climate_model_output'))
+        print(data_tcm.year[0])
+        assert data_tcm.year[0] == 1850
         
 if __name__ == '__main__':
     unittest.main()
