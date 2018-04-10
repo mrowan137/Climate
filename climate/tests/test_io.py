de47e0ecf6793ce9a91358e586157f964f18f3d2
Michael Edward Rowan <MRowan@Michaels-MacBook-Pro-25.local>
2018-04-10 01:48:33 -0400

    update data load test

--- climate/tests/test_io.py
+++ climate/tests/test_io.py
@@ -8,7 +8,7 @@ from unittest import TestCase
 
 class TestIo(TestCase):
     def test_data_io(self):
-        data = load_data(get_example_data_file_path('global_surface_temp_seaice_air_infer.txt'))
+        data = load_data_temp(get_example_data_file_path('global_surface_temp_seaice_air_infer.txt'))
         assert data.year[0] == 1850
         
 if __name__ == '__main__':
