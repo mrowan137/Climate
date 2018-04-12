18d53f6199e48f264683eabd800bbff2ebd1245e
bbullard <bbullard@g.harvard.edu>
2018-04-12 15:59:02 -0400

    Modified prior.py to give numerical values

--- climate/inference/prior.py
+++ climate/inference/prior.py
@@ -1,85 +1,47 @@
 import numpy as np
-
 from numpy import log, inf
 
-
-
 class Prior:
-
     """
-
     Prior Base class. This can store the shape parameters in the object instance 
-
     then be used as a function 
-
     """
-
-
 
     def __init__(self, *shapeparams):
-
         self.params = shapeparams
 
 
-
-
-
 class LogUniformPrior(Prior):
-
     """
-
-    Returns the value of the uniform prior at position x for range xmin to xmax
-
+    Returns log of the value of the uniform prior at position x
     """
-
-
 
     def __call__(self, x):
-
         lower, upper = self.params
-
-        return np.where(
-
-            np.logical_and(x <= upper, x >= lower), -log(upper - lower), -inf)
-
-
-
+        if (lower <= x <= upper):
+            return log(1 / (upper-lower))
+        else:
+            return -inf
 
 
 class LogJefferysPrior(Prior):
-
     """
-
-    Returns the value of the Jefferys prior at position x for range xmin to xmax
-
+    Returns log of the value of the Jefferys prior at position x
     """
-
-
 
     def __call__(self, x):
-
         lower, upper = self.params
+        if (lower <= x <= upper):
+            return -log(x) - log( log(upper/lower) )
+        else:
+            return -inf
 
-        return np.where(
 
-            np.logical_and(x <= upper, x >= lower),
-
-            -log(x) - log(log(upper / lower)), -inf)
-
-        
-
-class LogNormalPrior(Prior):
-
+class LogGaussianPrior(Prior):
     """
-
-    Returns the value of the Jefferys prior at position x for range xmin to xmax
-
+    Returns log of the value of the Gaussian prior at position x
     """
-
-
 
     def __call__(self, x):
-
         mu, sig = self.params
-
-        return -(x-mu)**2/(2*sig**2)
+        return -log(2*np.pi*sig**2)/2 - (x-mu)**2/(2*sig**2)
